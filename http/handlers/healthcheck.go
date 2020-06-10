9092f0a4e6e9145b9f03a380673f5dad2de91c78
Luis Eduardo Falcao <eduardo.cfalcao@gmail.com>
2020-06-09 23:16:15 -0300

    Adding host info in healthcheck endpoint

--- http/handlers/healthcheck.go
+++ http/handlers/healthcheck.go
@@ -3,19 +3,22 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 )
 
 type status struct {
 	Health string `json:"health"`
+	Host   string `json:host`
 }
 
 // Healthcheck is a handler to return the status of the service
 func Healthcheck(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-type", "application/json")
-
+	host, _ := os.Hostname()
 	var status = status{
 		Health: "alive",
+		Host:   host,
 	}
 
 	_ = json.NewEncoder(w).Encode(status)
