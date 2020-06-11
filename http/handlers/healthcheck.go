eb6f6a46455142e07b2d982b52e1201421feaf69
Luis Eduardo Falcao <eduardo.cfalcao@gmail.com>
2020-06-11 10:26:14 -0300

    Updating cd.yml

--- http/handlers/healthcheck.go
+++ http/handlers/healthcheck.go
@@ -4,13 +4,11 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
-	"time"
 )
 
 type status struct {
-	Health      string `json:"health"`
-	Host        string `json:host`
-	RequestTime string `json:requestTime`
+	Health string `json:"health"`
+	Host   string `json:host`
 }
 
 // Healthcheck is a handler to return the status of the service
@@ -19,9 +17,8 @@ func Healthcheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	host, _ := os.Hostname()
 	var status = status{
-		Health:      "alive",
-		Host:        host,
-		RequestTime: time.Now().String(),
+		Health: "alive",
+		Host:   host,
 	}
 
 	_ = json.NewEncoder(w).Encode(status)
