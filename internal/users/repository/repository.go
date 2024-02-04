40726cab940b8cab789537b9399b371be9656181
Luis Eduardo Falcão <eduardo.cfalcao@gmail.com>
2024-02-04 17:28:28 -0300

    Add repository in users package

---
+++ internal/users/repository/repository.go
@@ -0,0 +1,21 @@
+package repositoy
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+)
+
+type Repositoy interface {
+	Get(ctx context.Context, id pgtype.UUID) (RawTransaction, error)
+	Tx(tx pgx.Tx) Repositoy
+}
+
+func (q *Queries) Tx(tx pgx.Tx) Repositoy {
+	return &Queries{
+		db: tx,
+	}
+}
+
+var _ Repositoy = (*Queries)(nil)
